724f6a10da809e55e70a9b9816b27acbe8acc956
mursalim9 <mursalim591@gmail.com>
2021-09-24 19:33:10 +0700

    File-File Latihan Pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Mursalim Ismail
+            saya tinggal di Kalideres
+            kegiatan yang saya sukai adalah
+            membaca komik, nonton filem dan 
+            bermain game.";
+    }
+}
